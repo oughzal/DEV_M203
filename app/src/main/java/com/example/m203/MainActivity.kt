5234d4091ed7a7ef290968e580f91c73afe5c353
omar OUGHZAL <omar.oughzal@gmail.com>
2025-10-21 15:01:16 +0100

    Init

--- app/src/main/java/com/example/m203/MainActivity.kt
+++ app/src/main/java/com/example/m203/MainActivity.kt
@@ -2,22 +2,33 @@ package com.example.m203
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.m203.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    val list = (1..1000).map { "Item $it"}.toMutableList()
 
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val adapter = ArrayAdapter<String>(this, R.layout.item_layout, list)
+        binding.lvNoms.adapter = adapter
+
+
 
     }
 }
