5b9ce0cfd3a756d6a7c54d989c7b18af3c15f10a
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-04 13:35:25 +0100

    init

--- app/src/main/java/com/example/m203/MainActivity.kt
+++ app/src/main/java/com/example/m203/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.m203
 
+import android.app.ActivityOptions
 import android.os.Bundle
+import android.transition.TransitionInflater
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.example.m203.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        window.enterTransition = TransitionInflater.from(this).inflateTransition(R.anim.slide_in_right)
+        window.exitTransition = TransitionInflater.from(this).inflateTransition(R.anim.slide_out_left)
+
+        binding.animateButton.setOnClickListener {
+            val anim = AnimationUtils.loadAnimation(this,R.anim.anim_scale )
+            binding.imgAnim.startAnimation(anim)
+        }
+
+        binding.btnToActivity.setOnClickListener {
+            val intent = android.content.Intent(this, MainActivity2::class.java)
+            val options = ActivityOptions.makeCustomAnimation(
+                this,
+                R.anim.slide_out_left,
+                R.anim.slide_in_right
+            )
+            val transition = ActivityOptions.makeSceneTransitionAnimation(this).toBundle()
+            startActivity(intent ,transition)
+        }
+
 
     }
 }
