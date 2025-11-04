315025c3824e1707e8ce50f89adb1ac3dda6d64b
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-04 14:49:51 +0100

    init

--- app/src/main/java/com/example/m203/MainActivity.kt
+++ app/src/main/java/com/example/m203/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.m203
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+
 import androidx.appcompat.app.AppCompatActivity
 import com.example.m203.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +17,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnViewAnimation.setOnClickListener {
+            val anim = AnimationUtils.loadAnimation(this,R.anim.anim1)
+            binding.btnViewAnimation.startAnimation(anim)
+            Snackbar.make(this,binding.root,"View Animation started",Snackbar.LENGTH_SHORT).show()
+        }
+
+
+        binding.btnObjectAnimation.setOnClickListener {
+            val objectAnimator1 = ObjectAnimator.ofFloat(
+                binding.imgOFPPT,
+                "rotationY",
+                0f,
+                0f,
+            )
+            objectAnimator1.duration = 2000
+               val objectAnimator2 = ObjectAnimator.ofFloat(
+                binding.imgOFPPT,
+                "rotation",
+                0f,
+                9000f,
+            )
+            objectAnimator2.duration = 20000
+//            objectAnimator2.interpolator =  android.view.animation.BounceInterpolator()
+
+            val animatorSet = AnimatorSet()
+            animatorSet.play(objectAnimator1).after(objectAnimator2)
+            animatorSet.start()
+
+        }
 
     }
 }
