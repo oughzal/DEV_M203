83048d476a5a22678b348dad8f1c015faf9f3f3e
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-05 13:24:43 +0100

    Commit

--- app/src/main/java/com/example/m03_compose/MainActivity.kt
+++ app/src/main/java/com/example/m03_compose/MainActivity.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.m03_compose.ui.theme.M03_composeTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,10 +42,32 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun MainScreen() {
-Text(
-    text = "Hello World",
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
 
-)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 30.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.car),
+            contentDescription = null,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .background(Color(0xFFEE0000))
+                .padding(10.dp)
+                .border(10.dp, Color.Black, CircleShape)
+                .background(Color.Green)
+                .padding(10.dp)
+                .border(20.dp, Color.Yellow, CircleShape)
+        )
+        Text(
+            text = "Hello World!!",
+            fontSize = 20.sp
+
+        )
+    }
 }
-
 
