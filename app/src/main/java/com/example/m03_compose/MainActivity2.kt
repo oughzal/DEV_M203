7332279a5c13a52142b68be418d19ae7c84f50bc
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-05 14:50:18 +0100

    Commit

--- app/src/main/java/com/example/m03_compose/MainActivity2.kt
+++ app/src/main/java/com/example/m03_compose/MainActivity2.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -27,6 +28,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 
@@ -102,7 +104,9 @@ fun DicesScreen(){
     Spacer(modifier = Modifier.height(20.dp))
     Image(
         painter = painterResource(dices[i]),
-        contentDescription = null
+        contentDescription = null,
+        modifier = Modifier
+            .clip(CircleShape)
     )
 
     Row(
