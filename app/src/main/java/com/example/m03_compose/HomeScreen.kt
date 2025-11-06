322bc07533503b5fbe13a195afb0092255b817cb
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-06 03:00:00 +0100

    Commit

---
+++ app/src/main/java/com/example/m03_compose/HomeScreen.kt
@@ -0,0 +1,132 @@
+package com.example.m03_compose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Call
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.PHONE
+)
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        modifier = Modifier.padding(10.dp),
+        topBar = { HomeTopBar() },
+        bottomBar = { HomeBottomBar() }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            CountriesScreen()
+        }
+
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar() {
+    TopAppBar(
+        title = { Text(text = "DEVOAM") },
+        navigationIcon = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = null
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun HomeBottomBar() {
+    NavigationBar(
+        containerColor = Color.Transparent,
+        tonalElevation = 0.dp
+    ) {
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    imageVector = Icons.Outlined.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Home",
+                    fontSize = 10.sp
+                )
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    imageVector = Icons.Outlined.Call,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Call",
+                    fontSize = 10.sp
+                )
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    imageVector = Icons.Outlined.Settings,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Settings",
+                    fontSize = 10.sp
+                )
+            }
+        )
+
+    }
+}
--- app/src/main/java/com/example/m03_compose/MainActivity.kt
+++ app/src/main/java/com/example/m03_compose/MainActivity.kt
@@ -4,36 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Devices.TABLET
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.m03_compose.ui.theme.M03_composeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            M03_composeTheme {
-
-                    MainScreen()
 
 
-            }
+            HomeScreen()
+
         }
     }
 }
@@ -42,19 +50,71 @@ class MainActivity : ComponentActivity() {
     showBackground = true,
     showSystemUi = true,
     device = Devices.PHONE
-    )
+)
 @Composable
 fun MainScreen() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+
+        ) {
+            CountriesScreen()
+
+        }
+    }
+}
+
+@Composable
+fun CountriesScreen() {
+    LazyColumn {
+        items(countries) { c ->
+            CountryScreen(c)
+        }
+
+    }
+
+}
+
+@Composable
+fun CountryScreen(country: Country) {
+    Row(
         modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
+            .fillMaxWidth()
+            .padding(10.dp)
+            .background(Color(0xFFEEEEEE))
+            .clip(RoundedCornerShape(20.dp))
+            .border(2.dp, Color.Gray)
+            .padding(16.dp)
+
+
     ) {
-        Text(
-            text = "Hello World"
+        Image(
+            painter = painterResource(country.imageId),
+            contentDescription = "Flag of ${country.name}",
+            modifier = Modifier
+                .size(100.dp)
+
         )
+        Column(
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(
+                text = country.name,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "${country.capital}",
+                fontSize = 16.sp
+            )
+
+        }
     }
 }
 
