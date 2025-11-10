ecbff7f6e340bd652e6265ce08ec4071e203c40d
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-10 14:03:40 +0100

    commit

--- app/src/main/java/com/example/m03_compose/MainActivity.kt
+++ app/src/main/java/com/example/m03_compose/MainActivity.kt
@@ -4,10 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -15,8 +28,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -40,21 +55,20 @@ class MainActivity : ComponentActivity() {
     )
 @Composable
 fun MainScreen() {
-    MaterialTheme {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-        ) {
-            Button(
-
-                onClick = {}
+    LazyHorizontalGrid(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top=80.dp)
+            .background(Color.LightGray),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        rows = GridCells.Fixed(7)
+    ) {
+        items(countries){ item ->
+            Image(
+                painter = painterResource(item.imageId),
+                contentDescription = null
             )
-            {
-                Text(text = "Hello World!")
-            }
         }
     }
 
