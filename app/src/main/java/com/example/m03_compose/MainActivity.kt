83edf8bc33dc6c9df277a9f549a8ab3e694c7cbd
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-11 13:56:54 +0100

    commit

--- app/src/main/java/com/example/m03_compose/MainActivity.kt
+++ app/src/main/java/com/example/m03_compose/MainActivity.kt
@@ -1,24 +1,39 @@
 package com.example.m03_compose
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.CameraAlt
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.More
-import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Button
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,11 +44,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
@@ -62,20 +86,69 @@ fun MainScreen() {
         topBar = { TopBarScreen() },
         bottomBar = { BottomBarScreen() }
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .padding(it)
-                .fillMaxSize()
-        ) {
-            Text(text = "${coutryFlag("MA")}",
-                fontSize = 80.sp
-            )
-        }
 
+        ConversationScreen(it)
     }
 
+}
+
+@Composable
+fun ConversationScreen(padding: PaddingValues) {
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(6.dp),
+        modifier = Modifier
+            .padding(padding)
+            .fillMaxSize()
+
+    ) {
+        items(conversations) { conversation ->
+            ConversationItem(conversation = conversation)
+        }
+    }
+}
+
+@Composable
+fun ConversationItem(conversation: Conversation) {
+    ElevatedCard(
+        colors = CardColors(
+            containerColor = Color.White,
+            contentColor = Color.Black,
+            disabledContainerColor = Color.Gray,
+            disabledContentColor = Color.Gray
+        ),
+        modifier = Modifier
+            .padding(start = 5.dp,end=5.dp)
+    ){
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+
+
+        ) {
+            Image(
+                painter = painterResource(conversation.profilImage), contentDescription = null,
+                modifier = Modifier
+                    .width(60.dp)
+                    .aspectRatio(1f)
+                    .clip(CircleShape)
+                    .border(BorderStroke(2.dp, Color.Green), shape = CircleShape)
+            )
+
+            Column {
+                Text(
+                    text = conversation.name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = conversation.lastMessage)
+                Text(text = conversation.time)
+            }
+        }
+    }
 }
 
 @Composable
@@ -132,6 +205,7 @@ fun BottomBarScreen() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBarScreen() {
+    var expanded by remember { mutableStateOf(false) }
     TopAppBar(
         navigationIcon = {
             IconButton(onClick = {}) {
@@ -149,11 +223,38 @@ fun TopBarScreen() {
                     contentDescription = null
                 )
             }
-            IconButton(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Default.MoreVert,
-                    contentDescription = null
-                )
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                IconButton(onClick = { expanded = !expanded }) {
+                    Icon(Icons.Default.MoreVert, contentDescription = "More options")
+                }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("Profil") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Person,
+                                contentDescription = null
+                            )
+                        },
+                        onClick = { /* Do something... */ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Setting") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Settings,
+                                contentDescription = null
+                            )
+                        },
+                        onClick = { /* Do something... */ }
+                    )
+                }
             }
         },
 
@@ -161,5 +262,7 @@ fun TopBarScreen() {
             .background(Color.Cyan)
     )
 }
+
+
 
 
